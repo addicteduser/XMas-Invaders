53c95d5d15bb1e445822f04dcc9199b778374436
addicteduser <tintin.kalaw@gmail.com>
2014-12-12 17:25:42 +0800

    Imported utility bar. Added them to scene. Buggy

--- XMas Invaders/Assets/Scripts/BoundsScroller.cs
+++ XMas Invaders/Assets/Scripts/BoundsScroller.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bounds : MonoBehaviour {
+public class BoundsScroller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ XMas Invaders/Assets/Scripts/UtilityBarScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UtilityBarScroller : MonoBehaviour {
+
+	private float distanceFromSanta;
+	
+	// Use this for initialization
+	void Start () {
+		distanceFromSanta = transform.position.x - Santa.currentPos.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newX = transform.position.x + distanceFromSanta;
+		float y = transform.position.y;
+		float z = transform.position.z;
+		transform.position = new Vector3(newX, y, z);		
+	}
+}
