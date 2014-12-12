7f46735c056d2e5a8a5f6fe8d11e8fa226d15938
addicteduser <tintin.kalaw@gmail.com>
2014-12-12 15:52:37 +0800

    fixed stuff

---
+++ XMas Invaders/Assets/Scripts/Bounds.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float y = transform.position.y;
+		float z = transform.position.z;
+		transform.position = new Vector3(Santa.currentPos.x, y, z);
+	}
+	
+}
---
+++ XMas Invaders/Assets/Scripts/CameraScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScroller : MonoBehaviour {
+
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed * Time.deltaTime, 0f, 0f);
+	}
+}
