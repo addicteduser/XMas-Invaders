19c93913faaad94725ad35064836cc39b40a27ee
addicteduser <tintin.kalaw@gmail.com>
2014-12-14 19:02:36 +0800

    try shooting enemies

--- XMas Invaders/Assets/Scripts/MoveTrail.cs
+++ XMas Invaders/Assets/Scripts/MoveTrail.cs
@@ -6,6 +6,8 @@ public class MoveTrail : MonoBehaviour {
 	// This is responsible for the movement of the bullets
 
 	public int moveSpeed = 20;
+	public int damage = 1;
+	public bool isEnemyShot = false;
 
 	void Update () {
 		transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
