7b30ab8a5597b5eb3c4f89524330461270ac0241
addicteduser <tintin.kalaw@gmail.com>
2014-12-14 20:15:04 +0800

    Weapons can now kill enemies.

--- XMas Invaders/Assets/Scripts/Health.cs
+++ XMas Invaders/Assets/Scripts/Health.cs
@@ -16,30 +16,29 @@ public class Health : MonoBehaviour {
 	/// Inflicts damage and check if the object should be destroyed
 	/// </summary>
 	/// <param name="damageCount"></param>
-	public void Damage(int damageCount)
-	{
+	public void Damage(int damageCount) {
 		hp -= damageCount;
 		
-		if (hp <= 0)
-		{
+		if (hp <= 0) {
 			// Dead!
 			Destroy(gameObject);
 		}
 	}
 	
-	void OnTriggerEnter2D(Collider2D otherCollider)
-	{
+	void OnTriggerEnter2D(Collider2D otherCollider)	{
 		// Is this a shot?
 		MoveTrail shot = otherCollider.gameObject.GetComponent<MoveTrail>();
-		if (shot != null)
-		{
+		if (shot != null) {
 			// Avoid friendly fire
-			if (shot.isEnemyShot != isEnemy)
-			{
+			if (shot.isEnemyShot != isEnemy) {
 				Damage(shot.damage);
 				
-				// Destroy the shot
-				Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+				Debug.Log(shot.gameObject.name);
+				
+				if (!shot.gameObject.name.Contains("GarlandWipeoutBulletTrail")) {
+					// Destroy the shot
+					Destroy(shot.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+				}
 			}
 		}
 	}
